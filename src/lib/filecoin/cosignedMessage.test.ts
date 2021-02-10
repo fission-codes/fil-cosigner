9213dfe9e0afc0258f99aef361e9d08ae6ec8014
Benjamin Bollen <ben@mosaiclabs.eu>
2021-02-10 18:01:22 +0100

    experimentation with test assertion patterns

--- src/lib/filecoin/cosignedMessage.test.ts
+++ src/lib/filecoin/cosignedMessage.test.ts
@@ -1,10 +1,10 @@
 import { CosignedLotusMessage } from './cosignedMessage';
 import { expect } from 'chai';
 
-describe('Cosigned Lotus Message', () => {
-  describe('validate struct into Cosigned Lotus Message', () => {
-    it('it should construct valid Lotus Message', () => {
-      expect(true);
-    });
-  });
-});
+// describe('Cosigned Lotus Message', () => {
+//   describe('validate struct into Cosigned Lotus Message', () => {
+//     it('it should construct valid Lotus Message', () => {
+//       expect(false);
+//     });
+//   });
+// });
--- src/lib/filecoin/message.test.ts
+++ src/lib/filecoin/message.test.ts
@@ -1,20 +1,67 @@
-import { castToLotusMessage, LotusMessage } from './message';
+import { castToLotusMessage, InvalidLotusMessage, LotusMessage } from './message';
 import { BigNumber } from 'bignumber.js';
-import { isLeft, isRight } from "fp-ts/lib/Either";
-import { expect } from 'chai';
+import { Either, isLeft, isRight, fold } from "fp-ts/lib/Either";
+import { constFalse, pipe } from 'fp-ts/function';
+import { assert } from 'chai';
+import P from 'pino';
 
-const baseLotusObject = {
+const incompleteBaseLotusObj = {
+  // version: 0,
   to: 't03832874859695014541',
   from: 't1pyfq7dg6sq65acyomqvzvbgwni4zllglqffw5dy',
   nonce: 10,
-  value: new BigNumber('11416382733294334924'),
-  method: 0
+  value: '11416382733294334924',
+  method: 0,
+  gasFeeCap: '1',
+  gasPremium: '1',
+  gasLimit: 123,
+  params: ''
 }
 
-describe('Lotus Message', () => {
-  describe('validate struct into Lotus Message', () => {
+const baseLotusObject = {
+  version: 0,
+  to: 't03832874859695014541',
+  from: 't1pyfq7dg6sq65acyomqvzvbgwni4zllglqffw5dy',
+  nonce: 10,
+  value: '11416382733294334924',
+  method: 0,
+  gasFeeCap: '1',
+  gasPremium: '1',
+  gasLimit: 123,
+  params: ''
+}
+
+describe('constructing a Lotus Message', () => {
+  describe('valid structures should cast', () => {
     it('it should construct valid Lotus Message', () => {
-      expect(isLeft(castToLotusMessage(baseLotusObject)));
+      assert((isRight(castToLotusMessage(baseLotusObject))),
+        'expected valid Lotus Message');
+    });
+  });
+
+  describe('invalid structures should fail', () => {
+    it('should fail if version is not 0', () => {
+      assertInvalidLotusMessage(
+        'version is a required field and has to be a number',
+        castToLotusMessage(incompleteBaseLotusObj));
     });
   });
 });
+
+
+const assertInvalidLotusMessage = (
+  expectedError: string,
+  result: Either<InvalidLotusMessage, LotusMessage>
+): void => {
+  pipe(result, fold(
+    (invalidMessage: InvalidLotusMessage): void => {
+      assert(
+        (expectedError === invalidMessage.message),
+        'Invalid Lotus Message error does not match. Received: ' +
+          invalidMessage.message + '; expected: ' +
+          expectedError);
+    },
+    (_lotusMessage: LotusMessage): void => {
+      assert(false, 'Got a valid Lotus Message, expected invalid message error.');
+    }));
+}
