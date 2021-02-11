8638cb5a71f50d2f834b614ab8a88fee7679545f
Benjamin Bollen <ben@mosaiclabs.eu>
2021-02-11 18:58:05 +0100

    towards testing marshalled signing bytes

--- src/lib/filecoin/message.test.ts
+++ src/lib/filecoin/message.test.ts
@@ -1,11 +1,11 @@
-import { castToLotusMessage, InvalidLotusMessage, LotusMessage } from './message';
+import { castToLotusMessage, signingBytesLotusMessage, InvalidLotusMessage, LotusMessage } from './message';
 import { BigNumber } from 'bignumber.js';
-import { Either, isLeft, isRight, fold } from "fp-ts/lib/Either";
-import { constFalse, pipe } from 'fp-ts/function';
+import { Either, isLeft, isRight, fold, getOrElseW } from "fp-ts/lib/Either";
+import { pipe } from 'fp-ts/function';
+import { readFileSync } from 'fs';
 import { assert } from 'chai';
-import P from 'pino';
 
-const incompleteBaseLotusObj = {
+const incompleteBaseLotusMessagObject = {
   // version: 0,
   to: 't03832874859695014541',
   from: 't1pyfq7dg6sq65acyomqvzvbgwni4zllglqffw5dy',
@@ -18,8 +18,8 @@ const incompleteBaseLotusObj = {
   params: ''
 }
 
-const baseLotusObject = {
-  version: 0,
+const baseLotusMessagObject = {
+  // version: 0,
   to: 't03832874859695014541',
   from: 't1pyfq7dg6sq65acyomqvzvbgwni4zllglqffw5dy',
   nonce: 10,
@@ -33,8 +33,8 @@ const baseLotusObject = {
 
 describe('constructing a Lotus Message', () => {
   describe('valid structures should cast', () => {
-    it('it should construct valid Lotus Message', () => {
-      assert((isRight(castToLotusMessage(baseLotusObject))),
+    it('should construct valid Lotus Message', () => {
+      assert((isRight(castToLotusMessage(baseLotusMessagObject))),
         'expected valid Lotus Message');
     });
   });
@@ -43,11 +43,30 @@ describe('constructing a Lotus Message', () => {
     it('should fail if version is not 0', () => {
       assertInvalidLotusMessage(
         'version is a required field and has to be a number',
-        castToLotusMessage(incompleteBaseLotusObj));
+        castToLotusMessage(incompleteBaseLotusMessagObject));
     });
   });
 });
 
+describe('signing bytes of a Lotus Message', () => {
+    // load test data
+    const testVector = JSON.parse(
+      readFileSync('./test-vectors/filecoin/getCid.json').toString());
+
+    it('should marshal correct bytes', () => {
+      assert(isRight(castToLotusMessage(testVector.signed_message.message)));
+      // pipe(castToLotusMessage(testVector.signed_message.message)), fold(
+      //   (error: InvalidLotusMessage): void => {
+      //     assert(false, 'expected valid Lotus Message')
+      //   },
+      //   (lotusMessage: LotusMessage): void => {
+      //     assert(false, 'what?');
+      //     console.log(lotusMessage);
+      //     const sbytes = signingBytesLotusMessage(lotusMessage);
+      //   }
+      // );
+  });
+});
 
 const assertInvalidLotusMessage = (
   expectedError: string,
