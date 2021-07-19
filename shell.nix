239dde07dba1d93ae5d62247a8153d121ca82bae
James Walker <walkah@walkah.net>
2021-07-19 11:54:44 -0400

    fix up nix + dependencies (#26)

--- shell.nix
+++ shell.nix
@@ -1,12 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.nodejs
-    pkgs.yarn
-
-    pkgs.nodePackages.npm
-    pkgs.nodePackages.yo
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+in pkgs.mkShell {
+  name = "fil-cosigner";
+  buildInputs = with pkgs; [
+    nodejs-16_x
+    (yarn.override { nodejs = nodejs-16_x; })
 
     # keep this line if you use bash
     pkgs.bashInteractive
