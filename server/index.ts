256ca5948a064a00de9bf4813671e5f42ea1979d
James Walker <walkah@walkah.net>
2021-07-20 16:47:07 -0400

    add pino-http logging

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express from 'express'
+import logger from 'pino-http'
 import bodyParser from 'body-parser'
 import bearerToken from 'express-bearer-token'
 import cors from 'cors'
@@ -21,11 +22,12 @@ app.use(
 app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || '100kb' }))
 app.use(bearerToken())
 app.use(cors())
+app.use(logger())
 
 // routes
 app.use('/api/v1/filecoin', filecoinRouter)
 
-// fire it up
+// fire it ups
 const port = parseInt(process.env.PORT)
 app.listen(port, () => {
   console.log(`up and running at at http://localhost:${port}`)
