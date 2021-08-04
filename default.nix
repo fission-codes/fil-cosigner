50e8360ff40e7ed75fd67882e092ad7a8518a929
James Walker <walkah@walkah.net>
2021-08-04 15:28:23 -0400

    enable nix builds

---
+++ default.nix
@@ -0,0 +1,33 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+  yarn = pkgs.yarn.overrides { nodejs = pkgs.nodejs-16_x; };
+
+  package = pkgs.lib.importJSON ./package.json;
+  name = package.name;
+in pkgs.mkYarnPackage {
+  src = ./.;
+  doDist = false;
+  dontStrip = true;
+
+  packageJSON = ./package.json;
+  yarnLock = ./yarn.lock;
+  yarnNix = ./nix/yarn.nix;
+
+  buildPhase = ''
+    export HOME=$(mktemp -d)
+    yarn --offline build
+  '';
+
+  # doDist is apparently broken in mkYarnPackage
+  distPhase = ''
+    true
+  '';
+
+  postInstall = ''
+    makeWrapper ${pkgs.nodejs-16_x}/bin/node "$out/bin/${name}" \
+      --add-flags "$out/libexec/${name}/deps/${name}/dist/index.js"
+  '';
+
+  nativeBuildInputs = with pkgs; [ makeWrapper ];
+}
